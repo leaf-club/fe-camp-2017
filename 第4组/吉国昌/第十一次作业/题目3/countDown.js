676a832083f206d3652f5bcdd39747fd59b0e546
JGC-HUST <a1344143728@163.com>
2018-03-14 13:10:58 +0800

    吉国昌第11次作业
    fix up

--- 第4组/吉国昌/第十一次作业/题目3/countDown.js
+++ 第4组/吉国昌/第十一次作业/题目3/countDown.js
@@ -1,15 +1,16 @@
-var countDown = function() {
+var countDown = function () {
   var now = new Date();
   var a = now.getTime();
   var l = [];
-  for (var i = 1;i < 29;i++) {
+  for (var i = 1; i < 29; i++) {
     date = '2/' + i + '/2017';
     //onsole.log(date);
     var temp = new Date(Date.parse(date));
     //console.log(typeof temp);
     if (temp.getDay() === 3) { //getYime方法返回0-6（0指的是周日）
-      l.push('{date:'+'\'2017/2/' + i + '\', daysAfter: ' + Math.floor((a-temp.getTime())/1000/60/60/24) +' }')
+      l.push('{date:' + '\'2017/2/' + i + '\', daysAfter: ' + Math.floor((a - temp.getTime()) / 1000 / 60 / 60 / 24) + ' }')
     }
   }
   return l;
 }
+countDown();
