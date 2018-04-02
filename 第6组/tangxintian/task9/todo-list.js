ee22628818d5246516c664a313733c36d7eff428
logtxt <U201616080@hust.edu.cn>
2018-04-02 09:50:25 +0800

    todolist-1
    搭建环境，链接mysql数据库

--- 第6组/tangxintian/task9/todo-list.js
+++ 第6组/tangxintian/task9/todo-list.js
@@ -34,6 +34,7 @@ function Todo() {
         input.addEventListener('keypress', function (e) {
             if (e.keyCode === 13) {
                 that.add(input.value);
+                input.value ='';
                 that.render("all-js");
             }
         });
